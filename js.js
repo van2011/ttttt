763c8fa0919ad9aea47fb8d2184e9d4a60aceeed
van2011 <van2011111111111@gmail.com>
2024-01-28 19:25:59 +0200

    jaba

--- js.js
+++ js.js
@@ -1,3 +1,11 @@
 let express = require("express")
 let app = express()
 let port = 3000
+
+app.get("/", (req, res)=>{
+res.send("get")
+})
+
+app.listen(port, ()=>{
+console.log(`server running on port ${port}`)
+})
